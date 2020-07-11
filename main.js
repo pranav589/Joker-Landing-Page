0b19e44e2327523bd92db1fffcb8cff72bccdad1
pranav589 <pranav.bakale@gmail.com>
2020-07-11 23:01:51 +0530

    joker

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggle(){
+  var trailer=document.querySelector('.trailer');
+  var video=document.querySelector('video')
+  trailer.classList.toggle('active');
+  
+  video.currentTime='0';
+  video.pause()
+}
